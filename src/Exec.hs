dedb36fc1ff3ce990598989a4771e1ed507b083e
Heather <heather@live.ru>
2015-03-19 11:39:11 +0400

    switch to rawSystem

--- src/Exec.hs
+++ src/Exec.hs
@@ -5,12 +5,28 @@ module Exec
   ) where
 
 import System.Directory (setCurrentDirectory)
-import System.Process (waitForProcess, system)
+import System.Process (waitForProcess, rawSystem)
+
+import Data.List.Split
 
 import Control.Eternal.Syntax
 
+--TODO: quotes support
+execute :: [String] → IO()
+execute [] = return ()
+execute [x] = 
+    let qqq = filter (/= '\"') x -- temporary just filter it out
+        spl = filter (not . null) $ splitOn " " qqq
+        fs = spl !! 0
+        sn = tail spl
+    in rawSystem fs sn ≫ return ()
+execute (x:xs) = do execute [x]
+                    execute xs
+
 exec :: String → IO()
-exec args = system args ≫ return ()
+exec args = 
+    let commands = splitOn "&" args
+    in execute commands
 
 exc :: String → String → IO()
 exc path args = setCurrentDirectory path ≫ exec args
